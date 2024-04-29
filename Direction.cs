b98549ed54079d6354fd300caaf9fe1b7bef1af9
nikinenov1601 <nikinenov1@abv.bg>
2024-04-29 15:46:57 +0300

    Adding files for the snake game

---
+++ Direction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Snake
+{
+    public class Direction
+    {
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+        public Direction(int rowOffset, int columnOffset)
+        {
+            RowOffset = rowOffset;
+            ColumnOffset = columnOffset;
+        }
+
+        public int RowOffset { get; }
+        public int ColumnOffset { get; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   RowOffset == direction.RowOffset &&
+                   ColumnOffset == direction.ColumnOffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColumnOffset);
+        }
+
+        public Direction Opposite()
+        {
+            return new Direction(-RowOffset, -ColumnOffset);
+        }
+    }
+}
